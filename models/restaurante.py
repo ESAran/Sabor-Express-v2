c2b78a47c16df9de43099c48ca05a6a60896aa80
ESAran <du_aran@hotmail.com>
2024-03-19 17:23:09 -0300

    Definindo Construtur e str
    Utilizando métodos __init__ com self e __str__ para fazer definições da classe

--- models/restaurante.py
+++ models/restaurante.py
@@ -1,15 +1,28 @@
+# Criação da Classe
 class Restaurante:
-    nome = ''
-    categoria = ''
-    ativo = False
+    restaurantes = []
 
-restaurante_manu = Restaurante()
-restaurante_manu.nome = "Manu's Risottos"
-restaurante_manu.categoria = 'Risotos e Massas'
+    # Construtor: define os argumentos que necessita para criação do Objeto.
+    def __init__(self, nome, categoria):
+        self.nome = nome
+        self.categoria = categoria
+        self.ativo = False
+        Restaurante.restaurantes.append(self)
 
-restaurante_dudu = Restaurante()
+    # Método que determina como o Objeto vai ser exibido como string
+    def __str__(self):
+        return f'{self.nome}, {self.categoria}'
+    
+    def listar_restaurantes():
+        for restaurante in Restaurante.restaurantes:
+            estado = 'Em funcionamento' if restaurante.ativo == True else 'Fechado' 
+            print(f'''{restaurante.nome}
+ - Categoria: {restaurante.categoria}
+ - {estado}''')
 
-restaurantes = [restaurante_manu, restaurante_dudu]
+restaurante_manu = Restaurante("Manu's Risottos", 'Risotos e Massas')
 
-print(restaurante_manu.ativo)
+restaurante_dudu = Restaurante('Pizzaranria', 'Pizzaria')
 
+print('')
+Restaurante.listar_restaurantes()
